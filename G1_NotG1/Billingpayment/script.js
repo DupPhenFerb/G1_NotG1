343514a88a1152710e263c06a4427c9dca0e1649
DupPhenFerb <sonvhth2302009@fpt.edu.vn>
2023-09-18 07:21:24 +0700

    ...

---
+++ G1_NotG1/Billingpayment/script.js
@@ -0,0 +1,15 @@
+var navLinks = document.getElementById("navLinks");
+        function showMenu(){
+            navLinks.style.right = "0";
+        }
+        function hideMenu(){
+            navLinks.style.right = "-200px";
+        }
+let popup = document.getElementById("popup");
+function openPopup(){
+    popup.classList.add("open-popup")
+
+}
+function closePopup(){
+    popup.classList.remove("open-popup")
+}
